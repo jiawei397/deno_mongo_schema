d1c96920d142aa93fd207f0db5b6f15169dc158e
jw397 <jw397@126.com>
2023-04-27 17:17:40 +0800

    test: remove unused test_suite

--- test.deps.ts
+++ test.deps.ts
@@ -8,14 +8,3 @@ export {
 } from "https://deno.land/std@0.107.0/testing/asserts.ts";
 
 export const test = Deno.test;
-
-export {
-  afterAll,
-  afterEach,
-  // test,
-  beforeAll,
-  beforeEach,
-  describe,
-  it,
-  TestSuite,
-} from "https://deno.land/x/test_suite@0.9.1/mod.ts";
